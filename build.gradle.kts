491fc7ec3251c52511321376efd5fd151bea93a3
maia grotepass <maiatoday@gmail.com>
2021-06-01 21:41:14 +0200

    convert settings.gradle.kts
    convert root build.gradle.kts
    
    
    convert app build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,22 @@
+import kotlinx.coroutines.suspendAtomicCancellableCoroutine
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    ext {
-        compose_version = '1.0.0-beta07'
-    }
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
-        classpath "com.android.tools.build:gradle:7.1.0-alpha01"
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.32"
+        classpath("com.android.tools.build:gradle:7.1.0-alpha01")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.32")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks {
+    val clean by registering(Delete::class) {
+        delete(buildDir)
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,4 +7,4 @@ dependencyResolutionManagement {
     }
 }
 rootProject.name = "MagicSprinkles"
-include ':app'
+include(":app")
