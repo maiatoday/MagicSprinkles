456304f5ac8567834c8b65c41b01310dc53212e8
maia grotepass <maiatoday@gmail.com>
2021-06-06 17:56:00 +0200

    odd gradle things

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import kotlinx.coroutines.suspendAtomicCancellableCoroutine
-
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     val hiltPluginVersion by extra("2.36")
@@ -14,12 +12,6 @@ buildscript {
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
-    }
-}
-
-tasks {
-    val clean by registering(Delete::class) {
-        delete(buildDir)
     }
 }
 
