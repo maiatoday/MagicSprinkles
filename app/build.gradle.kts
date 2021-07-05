3c3259344480db2506dacce22d28c34a9a04459f
maia grotepass <maiatoday@gmail.com>
2021-07-05 22:48:38 +0200

    update versions and animation lib

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 //TODO clean up an put somewhere central
-val composeVersion = "1.0.0-beta07"
+val composeVersion = "1.0.0-beta09"
 val hiltVersion = "2.36"
 val hiltNavComposeVersion = "1.0.0-alpha02"
 
@@ -53,21 +53,22 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.5.0")
+    implementation("androidx.core:core-ktx:1.6.0")
     implementation("androidx.appcompat:appcompat:1.3.0")
     implementation("com.google.android.material:material:1.3.0")
     implementation("androidx.compose.ui:ui:$composeVersion")
     implementation("androidx.compose.material:material:$composeVersion")
     implementation("androidx.compose.ui:ui-tooling:$composeVersion")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
-    implementation("androidx.activity:activity-compose:1.3.0-alpha08")
-    implementation("androidx.navigation:navigation-compose:2.4.0-alpha01")
+    implementation("androidx.activity:activity-compose:1.3.0-rc01")
+    implementation("androidx.navigation:navigation-compose:2.4.0-alpha04")
+    implementation("androidx.compose.animation:animation:1.0.0-rc01")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$composeVersion")
     implementation("com.google.dagger:hilt-android:$hiltVersion")
     kapt("com.google.dagger:hilt-compiler:$hiltVersion")
-    implementation("androidx.hilt:hilt-navigation-compose:1.0.0-alpha02")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0-alpha03")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.2")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:$composeVersion")
 }
