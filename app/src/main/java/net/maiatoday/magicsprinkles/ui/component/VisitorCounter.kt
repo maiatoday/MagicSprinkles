97c1b7c39b6367106a5607aee6760ec73458d3b3
maia grotepass <maiatoday@gmail.com>
2021-07-11 22:14:33 +0200

    Counter that animates separate elements

--- app/src/main/java/net/maiatoday/magicsprinkles/ui/component/VisitorCounter.kt
+++ app/src/main/java/net/maiatoday/magicsprinkles/ui/component/VisitorCounter.kt
@@ -11,38 +11,68 @@ import androidx.compose.animation.slideOutVertically
 import androidx.compose.animation.with
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.Button
 import androidx.compose.material.LocalTextStyle
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import net.maiatoday.magicsprinkles.ui.theme.MagicSprinklesTheme
+import kotlin.math.pow
 
-enum class CounterState {
-    INCREMENT,
-    DECREMENT
+
+@ExperimentalAnimationApi
+@Composable
+fun Counter(
+    modifier: Modifier = Modifier,
+    style: TextStyle = LocalTextStyle.current,
+    onClick: () -> Unit,
+    count: Int,
+    width: Int
+) {
+    val displayWidth = maxOf(3, width)
+    Row(
+        modifier = Modifier.clickable(
+            interactionSource = remember { MutableInteractionSource() },
+            indication = null,
+            onClick = onClick
+        ),
+    ) {
+        for (n in displayWidth - 1 downTo 0) {
+            CounterCell(
+                modifier = modifier,
+                style = style,
+                count = (count.toDouble() / 10.0.pow(n) % 10).toInt(),
+                width = 1
+            )
+        }
+    }
 }
 
 @ExperimentalAnimationApi
 @Composable
-fun VisitorCounter(
+fun CounterCell(
     modifier: Modifier = Modifier,
     style: TextStyle = LocalTextStyle.current,
+    count: Int,
+    width: Int
 ) {
-    var counterState by remember {mutableStateOf(CounterState.INCREMENT)}
-    var count by remember { mutableStateOf(0) }
     val numbersSlidingAnimation: AnimatedContentScope<Int>.() -> ContentTransform = {
         if (initialState > targetState) {
-            slideInVertically(initialOffsetY = { it }) + fadeIn() with slideOutVertically(targetOffsetY = { -it }) + fadeOut()
+            slideInVertically(initialOffsetY = { it }) + fadeIn() with slideOutVertically(
+                targetOffsetY = { -it }) + fadeOut()
         } else {
-            slideInVertically(initialOffsetY = { -it }) + fadeIn() with slideOutVertically(targetOffsetY = { it }) + fadeOut()
+            slideInVertically(initialOffsetY = { -it }) + fadeIn() with slideOutVertically(
+                targetOffsetY = { it }) + fadeOut()
         }
     }
     AnimatedContent(
@@ -50,27 +80,35 @@ fun VisitorCounter(
         transitionSpec = numbersSlidingAnimation
     ) { number ->
         Text(
-            modifier = modifier.clickable(
-                interactionSource = remember { MutableInteractionSource() },
-                indication = null,
-                onClick = { counterState = when (counterState) {
-                    CounterState.INCREMENT -> CounterState.DECREMENT
-                    CounterState.DECREMENT -> CounterState.INCREMENT
-                } }
-            ),
-            text = number.toString().padStart(10, '0'),
+            modifier = modifier,
+            text = number.toString().padStart(width, '0'),
             style = style
         )
     }
 }
 
 @ExperimentalAnimationApi
-@Preview(showBackground = true, name = "VisitorCounter preview day")
+@Preview(showBackground = true, name = "Counter preview day")
 @Composable
-private fun PreviewVisitorCounter() {
+private fun PreviewCounter() {
     MagicSprinklesTheme {
+        var count by remember { mutableStateOf(99999) }
+        var isIncrementing by remember { mutableStateOf(true) }
         Surface(color = MaterialTheme.colors.background) {
-            VisitorCounter()
+
+            Column {
+                Button(onClick = { if (isIncrementing) count++ else count-- }) {
+                    Text("change")
+                }
+                Counter(
+                    count = count,
+                    width = 5,
+                    onClick = { if (isIncrementing) count++ else count-- })
+                Button(onClick = { isIncrementing = !isIncrementing }) {
+                    val label = if (isIncrementing) "👇" else "👆"
+                    Text(label)
+                }
+            }
         }
     }
 }
